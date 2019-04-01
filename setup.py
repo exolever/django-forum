9c0a58e1d3fa4f266e368d69ff96050da3e5c565
marfyl <marfyl.dev@gmail.com>
2019-04-01 11:23:56 +0200

    Update requirements

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
     name='django-forum',
     version=version,
     description='Implement forum for Django',
-    long_description=readme + '\n\n' + history,
+    long_description=readme,
     author='marfyl',
     author_email='marfyl.dev@gmail.com',
     url='https://github.com/exolever/django-forum',
